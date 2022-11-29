57de53422c6970bdaa44c47b6d0ef4b0d2bcd776
frank <francico.graya26@gmail.com>
2022-11-29 16:39:39 -0600

    se agrego index controller apuntando a index html

--- src/main/java/com/example/demo/Controller/IndexController.java
+++ src/main/java/com/example/demo/Controller/IndexController.java
@@ -4,19 +4,18 @@
  */
 package com.example.demo.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 /**
  *
  * @author franc
  */
 
-@RestController
+@Controller
 public class IndexController {
 
     @GetMapping("/hello")
     public String index() {
-        return "hola mundo";
+        return "index";
     }
 }
